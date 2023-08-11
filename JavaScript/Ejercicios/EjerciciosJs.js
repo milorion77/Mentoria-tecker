c4e0588b42df5542d76742f8b2359479122dec06
camilo <camilo.g7@hotmail.com>
2023-08-11 15:03:24 -0500

    Viernes javascript terminamos proyecto ejercicios

--- JavaScript/Ejercicios/EjerciciosJs.js
+++ JavaScript/Ejercicios/EjerciciosJs.js
@@ -1,3 +1,4 @@
+console.log("========================================Ejercicio 1  =============================================")
 /*Ejercicio 1
 Crear una función mkDate que reciba los parámetros day, month, year y retorne un objeto con dicha información.
 Agregarle además un método printDate que imprima por consola la fecha en formato dia/mes/año. */
@@ -16,8 +17,11 @@ function mkDate(day, month, year) {
   const fecha = mkDate(10, 8, 2023);
   fecha.printDate(); 
 
+console.log("========================================Ejercicio 2 =============================================")
 /* Ejercicio 2
-Modificar el objeto que retorna la función y agregar el método isValid. isValid debe validar que los tres campos estén seteados y que 1 <= day <= 31, 1 <= month <= 12, 1970 <= year < 3000. printDate deberá imprimir fecha inválida si la fecha no es válida. */  
+Modificar el objeto que retorna la función y agregar el método isValid. isValid debe validar que los 
+//tres campos estén seteados y que 1 <= day <= 31, 1 <= month <= 12, 1970 <= year < 3000.
+// printDate deberá imprimir fecha inválida si la fecha no es válida. */  
 function createDate(day, month, year) {
 function printDate() {
         if (isValid()) {
@@ -52,8 +56,11 @@ const invalidDate = createDate(40, 13, 2025);
 invalidDate.printDate();  // Imprimirá "Fecha inválida"
 console.log(invalidDate.isValid());  // Imprimirá false
 
+console.log("========================================Ejercicio 3 =============================================")
 /*Ejercicio 3
-Seguir modificando el objeto... agregar los métodos isBefore e isAfter que reciban otro objeto date como parámetro y retornen un boolean indicando si la fecha pasada como parámetro es anterior y posterior respectivamente. */
+Seguir modificando el objeto... agregar los métodos isBefore e isAfter que reciban otro 
+objeto date como parámetro y retornen un boolean indicando si la fecha pasada como parámetro 
+es anterior y posterior respectivamente. */
 
 function createDate(day, month, year) {
     function printDate() {
@@ -107,8 +114,11 @@ console.log(date2.isBefore(date1));  // Imprimirá false
 console.log(date2.isAfter(date1));   // Imprimirá true
 
 
+console.log("========================================Ejercicio 5 =============================================")
 /* Ejercicio 5
-Crea un array para albergar al menos 10 números enteros cualquiera , luego rellena el array (o crealo ya con los valores). El ejercicio trata de crear a partir de este array otros dos uno con los números pares y otro con los impares. No debes usar bucles, usa el método del array qe creas más apropiado.
+Crea un array para albergar al menos 10 números enteros cualquiera , luego rellena el array (o crealo ya con los valores). 
+El ejercicio trata de crear a partir de este array otros dos uno con los números pares y otro con los impares. No debes usar bucles,
+usa el método del array qe creas más apropiado.
 No debes usar bucles, mira el método más apropiado para crear un array a partir de otro.
 Un número es par si al dividirlo por 2 el resto es 0 (num%2 es 0) */
 
@@ -121,21 +131,21 @@ console.log("Números originales:", numeros);
 console.log("Números pares:", numerosPares);
 console.log("Números impares:", numerosImpares);
 
-
+console.log("========================================Ejercicio 6 =============================================")
 /*Ejercicio 6
 Crea un array de al menos 10 elementos para guardar números enteros. Usa un método para obtener la suma de los números pares y la de los números impares.
 Se trata de reducir el array a un número obtenido como suma de los pares en un caso y delos impares en el otro. */
 
-const numeros1 = [10, 23, 44, 57, 68, 71, 82, 95, 106, 119];
+const array = [32, 15, 88, 11, 8, 1, 2, 5, 10, 19];
 
-const sumaPares = numeros1.reduce((acumulador, num) => {
+const sumaPares = array.reduce((acumulador, num) => {
     if (num % 2 === 0) {
         return acumulador + num;
     }
     return acumulador;
 }, 0);
 
-const sumaImpares = numeros1.reduce((acumulador, num) => {
+const sumaImpares = array.reduce((acumulador, num) => {
     if (num % 2 !== 0) {
         return acumulador + num;
     }
@@ -147,32 +157,49 @@ console.log("Suma de números pares:", sumaPares);
 console.log("Suma de números impares:", sumaImpares);
 
 
-
+console.log("========================================Ejercicio 7 =============================================")
 /*Ejercicio 7
-Vamos a crear un objeto sencillo que se usa para guardar información sobre las calificaciones de un alumno. El curso contiene tres materias: Ingles, programación y HTM, y el objeto contendrá el nombre del alumno y la calificación en cada una de ellas. El script imprimirá el nombre y la media de sus calificaciones
+Vamos a crear un objeto sencillo que se usa para guardar información sobre las calificaciones de un alumno.
+El curso contiene tres materias: Ingles, programación y HTM, y el objeto contendrá el nombre del alumno y 
+la calificación en cada una de ellas. El script imprimirá el nombre y la media de sus calificaciones
 Por ejemplo, guardar las calificaciones de un alumno de nombre Juan, Inglés: 9, programacion: 8, HTML: 7. Sacará Nota media de Juan 8 */
 
+var alumno = {
+    nombre: "Juan",
+    ingles: 9,
+    programacion: 8,
+    HTML: 7
+  };
+  
+  var totalCalificaciones = alumno.ingles + alumno.programacion + alumno.HTML;
+  var notaMedia = totalCalificaciones / 3;
+  
+  console.log("Nombre del alumno:", alumno.nombre);
+  console.log("Nota media de", alumno.nombre + ":", notaMedia);
+  
+
+console.log("========================================Ejercicio 8 =============================================")
+console.log("este ejercicio esta en el html")
 
 
-/*Ejercicio 8
-Define un objeto, mediante una expresión, que tenga dos propiedades: precio y descuento y un método neto. El método calculará el precio con el descuento aplicado. Los valores se pedirán por teclado
-Por ejemplo objeto vestido, precio 400 y descuento 10. El método devolverá como resultado 360 (400 - 10*400/100). */
+console.log("========================================Ejercicio 10 =============================================")
+console.log("este ejercicio esta en el html")
 
-/*Ejercicio 10
-Realiza un ejercicio en js que pida una cadena de texto y la muestre poniendo el signo – entre cada carácter sin usar el método replace. */
+console.log("========================================Ejercicio 11 =============================================")
+console.log("este ejercicio esta en el html")
 
-/*Ejercicio 11
-Pedimos una cadena de texto que sabemos que puede contener paréntesis. Realiza un script que extraiga la cadena que se encuentra entre los paréntesis.
-Ej: Si escribimoe el texto “Hola (que) tal” se mostrará “que”. Si no existe el signo “(“ mostrará una cadena vacía y si existe el signo “(“pero no el signo “)” mostrará desde el primer paréntesis hasta el final. */
 
-/*Ejercicio 12
-Realiza un script que pida una cadena de texto y la devuelva al revés.
-Ej: si tecleo “hola que tal” deberá mostrar “lat euq aloh”. */
+console.log("========================================Ejercicio 12 =============================================")
+console.log("este ejercicio esta en el html")
+console.log("========================================Ejercicio 13 =============================================")
 
 /*Ejercicio 13
 Realiza un script que pida un texto e indique si es un palíndromo.
 Ejemplo de palíndromo: “Dabale arroz a la zorra el abad”.
-Palíndromo, 1. m. Palabra o frase que se lee igual de izquierda a derecha, que de derecha a izquierda; p. ej., anilina; dábale arroz a la zorra el abad. */
+Palíndromo, 1. m. Palabra o frase que se lee igual de izquierda a derecha,
+ que de derecha a izquierda; p. ej., anilina; dábale arroz a la zorra el abad. */
 
+
+ console.log("========================================Ejercicio 14 =============================================")
 /*Ejercicio 14
 Realizar acciones de carrusel con eventos */
