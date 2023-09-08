11ac4637368985fe58f1749f71c240f1a8334a00
camilo <camilo.g7@hotmail.com>
2023-09-08 14:54:48 -0500

    actualizacion de ejercicios del dia viernes

--- JavaScript/Ejercicios/taller.js
+++ JavaScript/Ejercicios/taller.js
@@ -60,3 +60,66 @@ while (!resultado.done) {
 }
 console.log(resultado); // Imprimir el resultado final
 
+
+console.log("============Ejercicio 3 ====================");
+
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+//promt
+rl.question('Ingresa un número entero: ', (numero) => {
+  // Convierte la entrada del usuario a un número entero
+  numero = parseInt(numero);
+
+  // Verifica si el número ingresado es un entero válido
+  if (!isNaN(numero)) {
+    // Itera desde 1 hasta el número ingresado
+    for (let asterisco = 1; asterisco <= numero; asterisco++) {
+      // Crea una cadena de asteriscos con la longitud actual
+      const linea = '*'.repeat(asterisco);
+      console.log(linea);
+    }
+  } else {
+    console.log('Debes ingresar un número entero válido.');
+  }
+
+  rl.close();
+});
+
+console.log("============Ejercicio 3.1 While, for  ====================");
+
+
+function calcularEdad(año, mes, dia) {
+  const hoy = new Date();
+  const nacimiento = new Date(año, mes - 1, dia); // Restamos 1 a mes para que sea el índice correcto (enero es 0, febrero es 1, etc.)
+  
+  // Calculamos la diferencia en milisegundos entre las dos fechas
+  const diferenciaEnMilisegundos = hoy - nacimiento;
+
+  // Convertimos la diferencia en milisegundos a años
+  const milisegundosEnUnAño = 1000 * 60 * 60 * 24 * 365.25; // Aproximadamente 365.25 días en un año, considerando años bisiestos
+  const edad = diferenciaEnMilisegundos / milisegundosEnUnAño;
+
+  console.log(`Usted tiene ${edad.toFixed(1)} años`);
+}
+
+// Ejemplo de uso
+calcularEdad(1996, 10, 7); // Reemplaza estos valores con tu fecha de nacimiento
+
+function edad(año,mes,dia){
+  const hoy = new Date();
+  console.log(hoy)
+  const nacimiento = new Date(año,mes,dia)
+  console.log(nacimiento)
+  const edad = (((((((hoy - nacimiento)/100)/60)/60)/24)/30)/12)
+
+
+console.log(`Usted tiene ${edad} años`)
+
+}
+
